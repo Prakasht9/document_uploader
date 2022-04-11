ce0b4239709c9c4bdf57d078169cfa79dbf71dd1
Prakash Thakur <prakasht9@hotmail.com>
2022-04-11 17:01:11 +0530

    testing

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,6 +11,7 @@ class Post < ApplicationRecord
   
   def correct_avatar_mime_type
     if avatar.attached? && !avatar.content_type.in?(%w(image/png image/jpeg image/jpg image/gif))
+      # testing
       avatar.purge
       errors.add(:avatar, 'Please upload only PNG, JPEG, JPG and GIF image formats')
     end
